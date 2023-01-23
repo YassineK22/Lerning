18a972ff2590397a9bea81e777a8fd6409df8351
YassineK22 <yassinekraiem22@gmail.com>
2023-01-23 06:01:38 +0100

    c/c and htmlCssJs repository

---
+++ C_C++/HelloWorld.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+
+
+
+
+
+
+
+
+// #include <string.h> //For fgets() And strlen()
+// int main(){
+//     printf("What's your name ?\n");
+//     char name[25];
+//     scanf("%s", &name);
+//     fgets(name , 25 , stdin);
+//     name[strlen(name)-1] = '\0';
+//     printf("Hello %s,How old are you ?\n", name);
+//     int age;
+//     scanf("%d", &age);
+//     printf("You are %d years old !!", age);
+//     return 0;
+// }
